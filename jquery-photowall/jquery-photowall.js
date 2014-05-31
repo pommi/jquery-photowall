3febe52be99ea1aa70af09d33d388b0322b43aed
Pim van den Berg <pim@nethuis.nl>
2014-05-31 12:30:46 +0200

    fix width:NaN in pw-line div

--- jquery-photowall/jquery-photowall.js
+++ jquery-photowall/jquery-photowall.js
@@ -229,9 +229,9 @@ var PhotoWall = {
             Create line of images and add it to container body.
         */
 		var showLine = function(line,total_width,last) {
+		    var num_photos = line.length;
 		    var ln = $("<div class='pw-line' style='width:"+(total_width+num_photos*PhotoWall.options.padding*2)+"'></div>")
                      .appendTo(PhotoWall._el);
-		    var num_photos = line.length;
 			var hCoef = (PhotoWall._c_width-num_photos*PhotoWall.options.padding*2) / total_width;
 			if(last)
 				var hCoef = 1;
